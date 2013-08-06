3b26e0f5b0317044b3f4cad2f4a9c43d31421f24
zhangyunlong <zhangyunlong@baidu.com>
2013-08-06 21:45:55 +0800

    fix bug

--- nodejs edition/baiduTemplate.js
+++ nodejs edition/baiduTemplate.js
@@ -1,5 +1,5 @@
 /**
- * baiduTemplate简单好用的Javascript模板引擎 1.0.5 版本
+ * baiduTemplate简单好用的Javascript模板引擎 1.0.6 版本
  * http://baidufe.github.com/BaiduTemplate
  * 开源协议：BSD License
  * 浏览器环境占用命名空间 baidu.template ，nodejs环境直接安装 npm install baidutemplate
@@ -13,12 +13,10 @@
 ;(function(){
 
     //取得浏览器环境的baidu命名空间，非浏览器环境符合commonjs规范exports出去
+    //修正在nodejs环境下，采用baidu.template变量名
     var baidu = typeof module === 'undefined' ? (this.baidu = this.baidu || {}) : module.exports;
 
-    //用来存储页面中的js代码(用于nodejs环境)
-    var jsStr = '';
-
-    //模板函数
+    //模板函数（放置于baidu.template命名空间下）
     baidu.template = function(str, data){
 
         //检查是否有该id的元素存在，如果有元素则获取元素的innerHTML/value，否则认为字符串为模板
@@ -39,8 +37,8 @@
                 };
 
                 //textarea或input则取value，其它情况取innerHTML
-                var tpl = /^(textarea|input)$/i.test(element.nodeName) ? element.value : element.innerHTML;
-                return bt._compile(tpl);
+                var html = /^(textarea|input)$/i.test(element.nodeName) ? element.value : element.innerHTML;
+                return bt._compile(html);
 
             }else{
 
@@ -51,20 +49,19 @@
 
         })();
 
-        //jsStr 是将页面中<script>标签中的js代码放回页面中
-        var html = fn(data);
-        html = html.replace("</body>",jsStr+"</body>");
-
         //有数据则返回HTML字符串，没有数据则返回函数 支持data={}的情况
-        var result = bt._isObject(data) ? html : fn;
-        html = null;
+        var result = bt._isObject(data) ? fn( data ) : fn;
         fn = null;
 
         return result;
     };
 
     //取得命名空间 baidu.template
-    bt = baidu.template;
+    var bt = baidu.template;
+
+    //标记当前版本
+    bt.versions = bt.versions || [];
+    bt.versions.push('1.0.6');
 
     //缓存  将对应id模板生成的函数缓存下来。
     bt.cache = {};
@@ -100,15 +97,15 @@
             .replace(/>/g,'&gt;')
             .replace(/"/g,'&quot;')
             .replace(/'/g,'&#39;')
-            .replace(/\\/g,'\\\\')
-            .replace(/\//g,'\\\/')
-            .replace(/\n/g,'\\n')
-            .replace(/\r/g,'\\r');
+            .replace(/\\\\/g,'\\')
+            .replace(/\\\//g,'\/')
+            .replace(/\\n/g,'\n')
+            .replace(/\\r/g,'\r');
     };
 
     //将字符串拼接生成函数，即编译过程(compile)
     bt._compile = function(str){
-        var funBody = "var _template_fun_array=[];\nvar fn=(function(data){\nvar _template_varName='';\nfor(name in data){\n_template_varName+=('var '+name+'=data[\"'+name+'\"];');\n};\neval(_template_varName);\n_template_fun_array.push('"+bt._analysisStr(str)+"');\n_template_varName=null;\n})(_template_object);\nfn = null;\nreturn _template_fun_array.join('');\n";
+        var funBody = "var _template_fun_array=[];\nvar fn=(function(__data__){\nvar _template_varName='';\nfor(var name in __data__){\n_template_varName+=('var '+name+'=__data__[\"'+name+'\"];');\n};\neval(_template_varName);\n_template_fun_array.push('"+bt._analysisStr(str)+"');\n_template_varName=null;\n})(_template_object);\nfn = null;\nreturn _template_fun_array.join('');\n";
         return new Function("_template_object",funBody);
     };
 
@@ -128,12 +125,7 @@
         var _left = bt._encodeReg(_left_);
         var _right = bt._encodeReg(_right_);
 
-        str = String(str).replace(/<script\s+type\s*=\s*['|"]text\/javascript['|"]\s*>[\s\S]*?<\/script>/g,function(item,$1){
-           jsStr += (item+'\n');
-           return '';
-        });
-        
-        str = str
+        str = String(str)
             
             //去掉分隔符中js注释
             .replace(new RegExp("("+_left+"[^"+_right+"]*)//.*\n","g"), "$1")
@@ -162,13 +154,15 @@
                     str = item;
                 }
                 return str ;
-            })
+            });
+
+
+        str = str 
+            //定义变量，如果没有分号，需要容错  <%var val='test'%>
+            .replace(new RegExp("("+_left+"[\\s]*?var[\\s]*?.*?[\\s]*?[^;])[\\s]*?"+_right,"g"),"$1;"+_right_)
 
             //对变量后面的分号做容错(包括转义模式 如<%:h=value%>)  <%=value;%> 排除掉函数的情况 <%fun1();%> 排除定义变量情况  <%var val='test';%>
             .replace(new RegExp("("+_left+":?[hvu]?[\\s]*?=[\\s]*?[^;|"+_right+"]*?);[\\s]*?"+_right,"g"),"$1"+_right_)
-
-            //定义变量，如果没有分号，需要容错  <%var val='test'%>
-            .replace(new RegExp("("+_left+"[\\s]*?var[\\s]*?.*?[\\s]*?[^;])[\\s]*?"+_right,"g"),"$1;"+_right_)
 
             //按照 <% 分割为一个个数组，再用 \t 和在一起，相当于将 <% 替换为 \t
             //将模板按照<%分为一段一段的，再在每段的结尾加入 \t,即用 \t 将每个模板片段前面分隔开
@@ -181,8 +175,7 @@
                 //找到 \t=任意一个字符%> 替换为 ‘，任意字符,'
                 //即替换简单变量  \t=data%> 替换为 ',data,'
                 //默认HTML转义  也支持HTML转义写法<%:h=value%>  
-                //.replace(new RegExp("\\t=(.*?)"+_right,"g"),"',typeof($1) === 'undefined'?'':baidu.template._encodeHTML($1),'");
-                .replace(new RegExp("\\t=(.*?)"+_right,"g"),"',typeof($1) === 'undefined'?'':$1,'");
+                .replace(new RegExp("\\t=(.*?)"+_right,"g"),"',typeof($1) === 'undefined'?'':baidu.template._encodeHTML($1),'");
         }else{
             str = str
                 
@@ -193,8 +186,7 @@
         str = str
 
             //支持HTML转义写法<%:h=value%>  
-            //.replace(new RegExp("\\t:h=(.*?)"+_right,"g"),"',typeof($1) === 'undefined'?'':baidu.template._encodeHTML($1),'")
-            .replace(new RegExp("\\t:h=(.*?)"+_right,"g"),"',typeof($1) === 'undefined'?'':$1,'")
+            .replace(new RegExp("\\t:h=(.*?)"+_right,"g"),"',typeof($1) === 'undefined'?'':baidu.template._encodeHTML($1),'")
 
             //支持不转义写法 <%:=value%>和<%-value%>
             .replace(new RegExp("\\t(?::=|-)(.*?)"+_right,"g"),"',typeof($1)==='undefined'?'':$1,'")
@@ -203,8 +195,7 @@
             .replace(new RegExp("\\t:u=(.*?)"+_right,"g"),"',typeof($1)==='undefined'?'':encodeURIComponent($1),'")
 
             //支持UI 变量使用在HTML页面标签onclick等事件函数参数中  <%:v=value%>
-            //.replace(new RegExp("\\t:v=(.*?)"+_right,"g"),"',typeof($1)==='undefined'?'':baidu.template._encodeEventHTML($1),'")
-            .replace(new RegExp("\\t:v=(.*?)"+_right,"g"),"',typeof($1)==='undefined'?'':$1,'")
+            .replace(new RegExp("\\t:v=(.*?)"+_right,"g"),"',typeof($1)==='undefined'?'':baidu.template._encodeEventHTML($1),'")
 
             //将字符串按照 \t 分成为数组，在用'); 将其合并，即替换掉结尾的 \t 为 ');
             //在if，for等语句前面加上 '); ，形成 ');if  ');for  的形式
